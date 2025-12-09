e502444bcc622d719a39c8604d79ab55c414a17f
Juan Forero <133827052+jsForeroBiomed@users.noreply.github.com>
2025-12-09 08:34:35 +0100

    Agregar config.js dado que todo lo que tiene es público y es un proyecto sencillo, no requiere seguridad mayor

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://tbkqruqtmwfmpicjthbq.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRia3FydXF0bXdmbXBpY2p0aGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNjAyMTksImV4cCI6MjA4MDgzNjIxOX0.1lwAG4dkJBIZ3AJgngzTlApyulubMs6Licdgm6PLfN0'
+};
